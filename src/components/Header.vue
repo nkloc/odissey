51c85e148d4b5747cf5994e982daf9600560f3d6
KLOC Nicolas <56578888+nkloc@users.noreply.github.com>
2021-06-30 13:10:42 +0200

    add: prettier format + mobile changes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,36 +1,47 @@
 <template>
-    <header>
-        <div>
-            <img src="../assets/logo.svg" alt="Logo d'odissey sécurité privée">
-            <h1>Odissey <br> <span>Sécurité Privée</span></h1>
-        </div>
-        <div>
-            <i @click.prevent="showMenu()" v-if="windowWidth < 749 && !burgerMenuActive" class="fas fa-bars burger"></i>
-            <i @click.prevent="showMenu()" v-if="windowWidth < 749 && burgerMenuActive" class="fas fa-times burger"></i>
-            <div v-if="windowWidth > 748" class="menu-mobile">
-                <ul> 
-                    <li> <a href="">Accueil</a></li>
-                    <li> <a href="">Nos services</a></li>
-                    <li> <a href="">Nous contacter</a></li>
-                    <li> <a href="">Nous rejoindre</a></li>
-                </ul>
-            </div>
-            <div v-if="burgerMenuActive" class="menu-mobile-fixed">
-                <ul> 
-                    <li> <a href="">Accueil</a></li>
-                    <li> <a href="">Nos services</a></li>
-                    <li> <a href="">Nous contacter</a></li>
-                    <li> <a href="">Nous rejoindre</a></li>
-                </ul>
-            </div>
-        </div>
-    </header>
+  <header>
+    <div>
+      <img src="../assets/logo.svg" alt="Logo d'odissey sécurité privée" />
+      <h1>
+        Odissey <br />
+        <span>Sécurité Privée</span>
+      </h1>
+    </div>
+    <div>
+      <i
+        @click.prevent="showMenu()"
+        v-if="windowWidth < 749 && !burgerMenuActive"
+        class="fas fa-bars burger"
+      ></i>
+      <i
+        @click.prevent="showMenu()"
+        v-if="windowWidth < 749 && burgerMenuActive"
+        class="fas fa-times burger"
+      ></i>
+      <div v-if="windowWidth > 748" class="menu-mobile">
+        <ul>
+          <li><a href="">Accueil</a></li>
+          <li><a href="">Nos services</a></li>
+          <li><a href="">Nous contacter</a></li>
+          <li><a href="">Nous rejoindre</a></li>
+        </ul>
+      </div>
+      <div v-if="burgerMenuActive" class="menu-mobile-fixed">
+        <ul>
+          <li><a href="">Accueil</a></li>
+          <li><a href="">Nos services</a></li>
+          <li><a href="">Nous contacter</a></li>
+          <li><a href="">Nous rejoindre</a></li>
+        </ul>
+      </div>
+    </div>
+  </header>
 </template>
 
 <style lang="stylus" scoped>
 @import '../css/variables'
 
-header 
+header
     display flex
     align-items center
     height 80px
@@ -39,7 +50,7 @@ header
     justify-content space-between
     padding 0 80px
     border-bottom 1px solid #E5E9F2
-    @media tablet 
+    @media tablet
         padding 0 50px
     @media mobile
         padding 0
@@ -56,15 +67,15 @@ h1
     text-align center
     @media mobile
         display none
-    span 
+    span
         position absolute
         font-size 12px
         font-weight regular
         color $black-color
-img 
+img
     height 50px
     margin-right 15px
-    @media mobile 
+    @media mobile
         position relative
         left 30px
 
@@ -80,33 +91,33 @@ ul
 
 .menu-mobile
     @media mobile
-        display none 
+        display none
 
 .menu-mobile-fixed
-    @media mobile 
-        position fixed 
+    @media mobile
+        position fixed
         left 0
         top 80px
         width 100%
         background $black-color
         height calc(100vh - 80px)
-        
+
 
 li
     display inline-block
     margin 0 15px
-    @media mobile 
+    @media mobile
         margin 20px 0
 
 
 a
     color $black-color
-    text-decoration none 
+    text-decoration none
     font-weight 500
     font-size 14px
     &:hover
         text-decoration underline
-    @media mobile 
+    @media mobile
         color $white-color
         font-size 25px
         font-weight bold
@@ -116,40 +127,40 @@ a
 
 .burger
     font-size 20px
-    @media mobile 
+    @media mobile
         position relative
         right 30px
 </style>
 
 <script>
 export default {
-    data() {
-        return {
-            windowWidth: 0,
-            burgerMenuActive: false,
-        }
-    },
+  data() {
+    return {
+      windowWidth: 0,
+      burgerMenuActive: false,
+    };
+  },
 
-    mounted() {
-        this.$nextTick(function() {
-            window.addEventListener('resize', this.getWindowWidth);
-            //Init
-            this.getWindowWidth()
-        })
-    },
+  mounted() {
+    this.$nextTick(function () {
+      window.addEventListener("resize", this.getWindowWidth);
+      //Init
+      this.getWindowWidth();
+    });
+  },
 
-    methods: {
-        getWindowWidth() {
-            this.windowWidth = document.documentElement.clientWidth;
-        },
-        showMenu(){
-            this.burgerMenuActive = !this.burgerMenuActive;
-        }
+  methods: {
+    getWindowWidth() {
+      this.windowWidth = document.documentElement.clientWidth;
     },
+    showMenu() {
+      this.burgerMenuActive = !this.burgerMenuActive;
+    },
+  },
 
-    beforeDestroy() {
-        window.removeEventListener('resize', this.getWindowWidth);
-        window.removeEventListener('resize', this.getWindowHeight);
-    }
-}
+  beforeDestroy() {
+    window.removeEventListener("resize", this.getWindowWidth);
+    window.removeEventListener("resize", this.getWindowHeight);
+  },
+};
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,51 +1,68 @@
 <template>
-<div>
+  <div>
     <section class="discover">
-    <div class="discover-text">
-      <h1>Sécuriser votre business</h1>
-      <h2>Odissey est un partenaire de groupe spécialisée dans la surveillance humaine basée en Île-De-France, dont l'ambition est d'offrir à ses clients un panel d'agents qualifiés qui vous protègeront des incendies, des agressions, des risques terroristes... </h2>
-      <a href="#agents">
-        <button>
-          Découvrir nos agents
-        </button>
-      </a>
-    </div>
-    <div class="container-img">
-      <img
-        src="../assets/security-guard.jpg"
-        alt="Photo d'un agent de sécurité"
-      />
-    </div>
-  </section>
-  <section class="agents" id="agents">
-    <div class="agents-text">
-      <h2>Nos différents profils d'agents</h2>
-    </div>
-    <div class="agents-profiles-container">
-      <article class="agents-profiles">
-        <h3>Agent SSIAP 1</h3>
-        <p>Les agents SSIAP1 ont pour mission d’assurer la sécurité des personnes et la sécurité incendie des biens.</p>
-      </article>
-      <article class="agents-profiles">
-        <h3>Agent SSIAP 1</h3>
-        <p>Les agents SSIAP1 ont pour mission d’assurer la sécurité des personnes et la sécurité incendie des biens.</p>
-      </article>
-      <article class="agents-profiles">
-        <h3>Agent SSIAP 1</h3>
-        <p>Les agents SSIAP1 ont pour mission d’assurer la sécurité des personnes et la sécurité incendie des biens.</p>
-      </article>
-      <article class="agents-profiles">
-        <h3>Agent SSIAP 1</h3>
-        <p>Les agents SSIAP1 ont pour mission d’assurer la sécurité des personnes et la sécurité incendie des biens.</p>
-      </article>
-      <article class="agents-profiles">
-        <h3>Agent SSIAP 1</h3>
-        <p>Les agents SSIAP1 ont pour mission d’assurer la sécurité des personnes et la sécurité incendie des biens.</p>
-      </article>
-
-    </div>
-  </section>
-</div>
+      <div class="discover-text">
+        <h1>Sécuriser votre business</h1>
+        <h2>
+          Odissey est un partenaire de groupe spécialisée dans la surveillance
+          humaine basée en Île-De-France, dont l'ambition est d'offrir à ses
+          clients un panel d'agents qualifiés qui vous protègeront des
+          incendies, des agressions, des risques terroristes...
+        </h2>
+        <a href="#agents">
+          <button>Découvrir nos agents</button>
+        </a>
+      </div>
+      <div class="container-img">
+        <img
+          src="../assets/security-guard.jpg"
+          alt="Photo d'un agent de sécurité"
+        />
+      </div>
+    </section>
+    <section class="agents" id="agents">
+      <div class="agents-text">
+        <h2>Nos différents profils d'agents</h2>
+      </div>
+      <div class="agents-profiles-container">
+        <article class="agents-profiles">
+          <h3>Agent SSIAP 1</h3>
+          <p>
+            Les agents SSIAP1 ont pour mission d’assurer la sécurité des
+            personnes et la sécurité incendie des biens.
+          </p>
+        </article>
+        <article class="agents-profiles">
+          <h3>Agent SSIAP 1</h3>
+          <p>
+            Les agents SSIAP1 ont pour mission d’assurer la sécurité des
+            personnes et la sécurité incendie des biens.
+          </p>
+        </article>
+        <article class="agents-profiles">
+          <h3>Agent SSIAP 1</h3>
+          <p>
+            Les agents SSIAP1 ont pour mission d’assurer la sécurité des
+            personnes et la sécurité incendie des biens.
+          </p>
+        </article>
+        <article class="agents-profiles">
+          <h3>Agent SSIAP 1</h3>
+          <p>
+            Les agents SSIAP1 ont pour mission d’assurer la sécurité des
+            personnes et la sécurité incendie des biens.
+          </p>
+        </article>
+        <article class="agents-profiles">
+          <h3>Agent SSIAP 1</h3>
+          <p>
+            Les agents SSIAP1 ont pour mission d’assurer la sécurité des
+            personnes et la sécurité incendie des biens.
+          </p>
+        </article>
+      </div>
+    </section>
+  </div>
 </template>
 
 <style lang="stylus" scoped>
