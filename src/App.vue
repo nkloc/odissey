73c912d1600a5ac1171c0683f580a90a3670ebaf
« Nicolas <n_kloc@etu-webschoolfactory.fr>
2025-04-18 15:01:30 +0200

    add: spinner loader

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,29 @@
 import LandingPage from '@/pages/LandingPage.vue'
 import logo from '@/assets/logo-texte.png'
 import logoTigre from '@/assets/logo-tigre.svg'
+import { ref, onMounted } from 'vue'
+import {FingerprintSpinner} from 'epic-spinners'
+
+// loader state
+const isLoaded = ref(false)
+
+onMounted(() => {
+  document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+      isLoaded.value = true
+    }
+  }
+})
 </script>
 
 <template>
+  <div class="loader-wrapper" v-if="!isLoaded">
+      <fingerprint-spinner
+        :animation-duration="1500"
+        :size="64"
+        color="#fff"
+      />
+    </div>
   <header>
     <img class="logo" alt="Odissey logo" :src="logo" />
   </header>
@@ -28,6 +48,17 @@ import logoTigre from '@/assets/logo-tigre.svg'
 <style lang="stylus" scoped>
 @import '/assets/variables.styl'
 
+.loader-wrapper
+  display: flex
+  width: 100vw
+  background: $black-color
+  z-index: 999
+  height: 100%
+  position: fixed
+  top: 0
+  left: 0
+  justify-content: center
+  align-items: center
 
 header
   display: flex
